068ef9416097e511e59c87fb4a25030bf4a3295b
Thomas Faure <thomas.faure@etu.univ-amu.fr>
2018-11-18 10:53:16 +0100

    modification sur puhshbullet.sh

--- pushbullet.sh
+++ pushbullet.sh
@@ -3,4 +3,4 @@
 API="o.OZwVI3zr98FrH5Exr0zKe6WkQ8uhqpeB"
 MSG="$1"
 
-curl -u $API: "https://api.pushbullet.com/v2/pushes -d type=note -d title="Alert" -d body="$MSG"
+curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="Alert" -d body="$MSG"
