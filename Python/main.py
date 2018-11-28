6e826c3b3e8ce7a49ecbb7471e30410aa433c273
Thomas <thomas.faure@etu.univ-amu.fr>
2018-11-29 00:46:29 +0100

    modification

--- Python/main.py
+++ Python/main.py
@@ -11,23 +11,45 @@ from ChangerCouleur import ChangerCouleur
 # main a recuperer sur le pc qui va appeler calculerRestantSopalin()
 
 def DeroulerFeuille(coef):
-	#print(coef)
 	fwd()
 	time.sleep(5)
 	stop()
 	time.sleep(4)
 	main()
 
+def calculerRestantSopalin():
+    ultrasonic_ranger = 7
+    valeurCapteur = 1
+    try:
+        valeurCapteur = ultrasonicRead(ultrasonic_ranger)  # mettre notre fonction capteur
+    except TypeError:
+        print "Error"
+    distanceCapteurRouleau = 30  # valeur constante a calculer
+    from ChangerCouleur import ChangerCouleur
+    ChangerCouleur(valeurCapteur)
+    if (valeurCapteur < distanceCapteurRouleau):
+        print("peut derouler")
+        from CalculerCoef import CalculerCoef
+        coef = CalculerCoef(valeurCapteur, distanceCapteurRouleau)
+        DeroulerFeuille(coef);
+    else:
+        print("peut pas derouler")
+
+
+
+
 def main():
 
     try:
+        print("test")
         ChangerCouleur(ultrasonicRead(7))
     except:
         print("le systeme ne peut fonctionner")
     capteur_main = 8
     main = False
-    valeur = -1
+    valeur = 11
     while (main == False):
+       
         try:
             valeur = ultrasonicRead(capteur_main)
         except:
@@ -35,8 +57,7 @@ def main():
         if (valeur < 10):
             print("main trouve")
             main = True
-	from CalculerRestantSopalin import calculerRestantSopalin
-    calculerRestantSopalin()
-
+            calculerRestantSopalin()
+	
 
 main()
